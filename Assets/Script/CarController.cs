3465a03dd8f2a762ff8359fac11c95f418f0caa5
Tornado <maxence.ribeau@gmail.com>
2024-01-30 19:02:04 +0100

    PhysicUpdate
    I have updated the physic and i added the script so that later i can bind a control to brake

--- Assets/Script/CarController.cs
+++ Assets/Script/CarController.cs
@@ -1,18 +1,109 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using PlayerControls;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.InputSystem;
 
 public class CarController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public enum Axel
+   {
+      Front,
+      Rear
+   }
+   
+   [Serializable] 
+   public struct Wheel
+   {
+      public GameObject WheelModel;
+      public WheelCollider wheelCollider;
+      public Axel axel;
+   }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public float maxAceleration = 30.0f;
+   public float brakeAcceleration = 50.0f;
+
+   public float turnSensitivity = 1.0f;
+   public float maxSteerAngle = 30.0f;
+
+   public Vector3 _centerOfMass;
+   
+   public List<Wheel> wheels;
+
+   public float moveInput;
+   public float steerInput;
+
+   private Rigidbody carRB;
+   private InputManager _controller;
+
+   private void Start()
+   {
+      //_controller = GetComponent<InputManager>();
+      carRB = GetComponent<Rigidbody>();
+      carRB.centerOfMass = _centerOfMass;
+   }
+
+   private void Update()
+   {
+      GetInputs();
+      AnimationsWheels();
+   }
+
+   private void FixedUpdate()
+   {
+      Move();
+      Steer();
+   }
+
+   void GetInputs()
+   {
+      moveInput = Input.GetAxis("Jump");
+      steerInput = Input.GetAxis("Horizontal");
+   }
+
+   void Move()
+   {
+      foreach (var wheel in wheels)
+      {
+         wheel.wheelCollider.motorTorque = moveInput * maxAceleration;
+      }
+   }
+
+   void Steer()
+   {
+      foreach (var wheel in wheels)
+      {
+         if (wheel.axel == Axel.Front)
+         {
+            var _steerAngle = steerInput * turnSensitivity * maxSteerAngle;
+            wheel.wheelCollider.steerAngle = Mathf.Lerp(wheel.wheelCollider.steerAngle, _steerAngle, 0.6f);
+         }
+      }
+   }
+
+   void Brake()
+   {
+      if(false)
+      {
+         foreach (var wheel in wheels)
+         {
+            wheel.wheelCollider.brakeTorque = 0;
+         }  
+      }
+   }
+
+   void AnimationsWheels()
+   {
+      foreach (var wheel in wheels)
+      {
+         Quaternion rotation;
+         Vector3 position;
+         wheel.wheelCollider.GetWorldPose(out position, out rotation);
+         wheel.WheelModel.transform.position = position;
+         wheel.WheelModel.transform.rotation = rotation;
+
+      }
+   }
 }
