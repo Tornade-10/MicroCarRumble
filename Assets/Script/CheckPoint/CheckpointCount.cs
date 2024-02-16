63af08c35184abf1cf54dbcd4d42ffa246e9a0d5
Tornado <maxence.ribeau@gmail.com>
2024-02-16 18:49:46 +0100

    WinScreen

--- Assets/Script/CheckPoint/CheckpointCount.cs
+++ Assets/Script/CheckPoint/CheckpointCount.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CheckpointCount : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public class CheckpointCount : MonoBehaviour
     private List<CheckPoint> _checkPoints;
     private int _nextCheckpointIndex;
     private int _numberOfTurns;
+
+    public GameObject player;
 
     private void Start()
     {
@@ -32,6 +35,19 @@ public class CheckpointCount : MonoBehaviour
         }
     }
 
+    public void Update()
+    {
+        if (_nextCheckpointIndex == _checkPoints.Count - 1)
+        {
+            _numberOfTurns++;
+        }
+
+        if (_numberOfTurns == 3)
+        {
+            SceneManager.LoadScene("VictoryScreen");
+        }
+    }
+
     public void PlayerGoThroughCheckpoint(CheckPoint checkPoint)
     {
         if (_checkPoints.IndexOf(checkPoint) == _nextCheckpointIndex)
@@ -42,6 +58,14 @@ public class CheckpointCount : MonoBehaviour
         else
         {
             Debug.Log("WrongCheckpoint");
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Respawn"))
+        {
+            player.transform.position = _nextCheckpointIndex - 1;
         }
     }
 }
